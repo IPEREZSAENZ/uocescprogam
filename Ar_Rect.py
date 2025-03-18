9a867c0025f987e460487c2a226fdce9f14b78ac
IPEREZSAENZ <ivan.perez.saenz12@gmail.com>
2025-03-18 22:57:11 +0100

    nuevas modificaciones

--- Ar_Rect.py
+++ Ar_Rect.py
@@ -1,4 +1,3 @@
-import unittest
 base = float(input("Ingresar base: "))
 altura = float(input("Ingresar Altura: "))
 
@@ -6,5 +5,16 @@ def calcular_area (base,altura):
     return base * altura
 
 resultado = calcular_area(base,altura)
-
 print("El resultado del área del rectángulo es: ", resultado)
+
+# En este caso realizaremos la prueba al código realizado anterioremente 
+#Arrange
+#Defininimos que testearemos la función llamada calcular_area.
+
+import unittest
+# Act - Acción
+def test_area():
+    print('test passed')
+    # Enviamos a la función calcular_area los valores 4 y 5 
+    assert calcular_area(4,5) == 20
+test_area()
